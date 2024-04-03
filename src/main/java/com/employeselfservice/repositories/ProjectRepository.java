35915a0f7727bc23e5d95da6d8c524f16a312ca5
rohanpansara <120650660+rohanpansara@users.noreply.github.com>
2024-04-03 17:23:19 +0530

    Add files via upload

--- src/main/java/com/employeselfservice/repositories/ProjectRepository.java
+++ src/main/java/com/employeselfservice/repositories/ProjectRepository.java
@@ -1,6 +1,5 @@
 package com.employeselfservice.repositories;
 
-import com.employeselfservice.models.Employee;
 import com.employeselfservice.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,9 +13,9 @@ public interface ProjectRepository extends JpaRepository<Project,Long> {
 
     // Method to get all projects an employee is working on
     @Query("SELECT pm.project FROM ProjectMember pm WHERE pm.employee.id = :employeeId")
-    List<Project> findAllProjectsByEmployeeId(@Param("employeeId") Long employeeId);
+    List<Project> findAllProjectsAssignedToTheEmployeeId(@Param("employeeId") Long employeeId);
 
     @Query("SELECT p FROM Project p WHERE p.owner.id = :employeeId")
-    List<Project> findAllProjectsOwnedByEmployee(@Param("employeeId") Long employeeId);
+    List<Project> findAllProjectsOwnedByTheEmployee(@Param("employeeId") Long employeeId);
 
 }

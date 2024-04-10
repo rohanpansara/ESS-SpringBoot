83a29ef7347f2399258a5f6589edd609f7f0cb3e
rohanpansara <120650660+rohanpansara@users.noreply.github.com>
2024-04-10 17:08:49 +0530

    Modified The APIs

--- src/main/java/com/employeselfservice/repositories/AttendanceRepository.java
+++ src/main/java/com/employeselfservice/repositories/AttendanceRepository.java
@@ -2,6 +2,7 @@ package com.employeselfservice.repositories;
 
 import com.employeselfservice.models.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -14,5 +15,6 @@ public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
     Optional<Attendance> findByEmployeeIdAndDate(Long employeeId, LocalDate date);
     List<Attendance> findByEmployeeId(Long employeeId);
 
-
+    @Query("SELECT a.workHours FROM Attendance a")
+    List<String> findAllWorkHours();
 }

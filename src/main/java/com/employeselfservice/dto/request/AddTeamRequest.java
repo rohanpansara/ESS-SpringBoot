590d8a54631f91e996069b1eab24037d38e498c2
rohanpansara <120650660+rohanpansara@users.noreply.github.com>
2024-04-22 17:07:05 +0530

    Event Module Started

---
+++ src/main/java/com/employeselfservice/dto/request/AddTeamRequest.java
@@ -0,0 +1,17 @@
+package com.employeselfservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddTeamRequest {
+    private String teamName;
+    private String teamDescription;
+}
---
+++ src/main/java/com/employeselfservice/dto/request/UpdateEmployeeRequest.java
@@ -0,0 +1,4 @@
+package com.employeselfservice.dto.request;
+
+public class UpdateEmployeeRequest {
+}
